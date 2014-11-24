192afff326b31ee2369abac19c80f6931fe13cfe
Bogdan Virtosu <virtosu.bogdan@gmail.com>
2014-11-24 23:28:59 +0200

    Demo: rendered questions.

---
+++ dev-tests-webapp/src/main/webapp/question.js
@@ -0,0 +1,42 @@
+Ext.application({
+    name: 'Question',
+
+    launch: function () {
+
+        Ext.define('question', {
+            extend: 'Ext.data.Model',
+            fields: [
+                {name: 'id', type: 'string'},
+                {name: 'name', type: 'string'},
+                {name: 'content', type: 'string'}
+            ]
+        });
+
+        var myQuestions = Ext.create('Ext.data.Store', {
+            model: 'question',
+            proxy: {
+                type: 'ajax',
+                url: 'devTests/demo/questions',
+                reader: {
+                    type: 'json',
+                    root: 'root',
+                    totalProperty: 'total'
+                }
+            },
+            autoLoad: true
+        });
+
+        Ext.create('Ext.grid.Panel', {
+            renderTo: Ext.getBody(),
+            bodyPadding: 5,
+            title: 'Questions',
+            store: myQuestions,
+            columns: [
+                {header: 'Id', dataIndex: 'id', flex: 1},
+                {header: 'Name', dataIndex: 'name', flex: 1},
+                {header: 'Content', dataIndex: 'content', flex: 1}
+            ]
+        });
+
+    }
+});
